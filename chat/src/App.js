f91256890c6ba1cfe261455a51bd7f2944c03381
Jonathan Dahnberg <Jonathan.MeeprongDahnberg@utb.ecutbildning.se>
2019-03-01 10:55:47 +0100

    first push

---
+++ chat/src/App.js
@@ -0,0 +1,82 @@
+import React, { Component } from 'react';
+import './App.css';
+import io from 'socket.io-client';
+
+const socket = io('http://ec2-13-53-66-202.eu-north-1.compute.amazonaws.com:3000');
+
+socket.on('connect', function(){
+  console.log("connected");
+});
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      username: "",
+      message: "",
+      messages: [],
+    }
+    this.onChange = this.onChange.bind(this);
+    this.onClick = this.onClick.bind(this);
+  }
+
+  componentDidMount() {
+    this.socket = io('http://ec2-13-53-66-202.eu-north-1.compute.amazonaws.com:3000');
+
+    socket.on("messages", (messages) => {
+      this.setState({ messages: messages });
+    });
+
+    socket.on("new_message", (message) => {
+      console.log("new message körs");
+      this.setState({ messages: [...this.state.messages, message]});
+    });
+  }
+
+  createLi(item){
+    console.log(item);
+    return(
+      <li className="chat__main__list__item" key={item.id}>
+        <p className="chat__main__list__item__username" >{item.username}</p>
+        <p className="chat__main__list__item__content">{item.content}</p>
+      </li>
+    )
+  }
+
+  onChange(e){
+    this.setState({message: e.target.value});
+  }
+
+  onClick(e){
+    console.log("onclick körs");
+
+    socket.emit("message", {
+      username: "jonathan",
+      content: this.state.message,
+    }, (response) => {
+        this.setState({ messages: [...this.state.messages, response.data.newMessage]});
+      console.log(response);
+    });
+
+
+  }
+
+  render() {
+    return (
+      <div className="chat">
+        <header className="chat__header"></header>
+        <main className="chat__main">
+          <ul className="chat__main__list">
+            {this.state.messages.map(item => this.createLi(item))}
+          </ul>
+        </main>
+        <div className = "chat__inputfield">
+          <input className="chat__inputfield__input" type="text" value={this.state.value} onChange={this.onChange}></input>
+          <button className="chat__inputfield__sendButton" onClick={this.onClick}>Send</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
